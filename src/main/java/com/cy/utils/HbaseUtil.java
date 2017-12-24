7f24c1d493959ad3360cb6b965f3b532b5b0db2e
“用户名” <>
2017-12-24 18:40:35 +0800

    s

--- src/main/java/com/cy/utils/HbaseUtil.java
+++ src/main/java/com/cy/utils/HbaseUtil.java
@@ -4,4 +4,7 @@ package com.cy.utils;
  * Created by 成圆 on 2017/12/24 16:27.
  */
 public class HbaseUtil {
+    public static void main(String[] args) {
+        System.out.println("hbase");
+    }
 }
