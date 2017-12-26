b7c8332a0e57fabb4a2babf353fefe828186bb1e
“用户名” <>
2017-12-26 20:31:38 +0800

    s

--- src/test/java/HbaseUtilTest.java
+++ src/test/java/HbaseUtilTest.java
@@ -1,8 +1,7 @@
 import com.cy.utils.HbaseUtil;
-import org.apache.hadoop.hbase.client.Connection;
 
 /**
- * Created by 成圆 on 2017/12/25 23:53.
+ * Created by cy on 2017/12/25 23:53.
  */
 public class HbaseUtilTest {
     public static void main(String[] args) {
